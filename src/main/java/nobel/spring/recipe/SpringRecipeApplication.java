26850f9be208632c8fa4142666ba0a5622af6912
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-01-21 00:20:09 +0200

    First Commit

---
+++ src/main/java/nobel/spring/recipe/SpringRecipeApplication.java
@@ -0,0 +1,13 @@
+package nobel.spring.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecipeApplication.class, args);
+    }
+
+}
