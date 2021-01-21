07b0c9b5383b351c465923f6b8fb22ccfdf51177
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-01-21 04:20:13 +0200

    Added Spring Repositories for domain Entities

--- src/main/java/nobel/spring/recipe/repositories/CategoryRepository.java
+++ src/main/java/nobel/spring/recipe/repositories/CategoryRepository.java
@@ -3,5 +3,8 @@ package nobel.spring.recipe.repositories;
 import nobel.spring.recipe.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/nobel/spring/recipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/nobel/spring/recipe/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package nobel.spring.recipe.repositories;
 import nobel.spring.recipe.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByUnit(String unit);
 }
